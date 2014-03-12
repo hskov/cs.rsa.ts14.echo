c9d8a10d2449c0df4f13cb5118bbb2bc1dffcd5e
hskov <hs@hskov.dk>
2014-03-12 11:01:48 -0400

    Added statemachine from group Golf

---
+++ ts14/src/cs/rsa/ts14/Golf/statemachine/EmptyState.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2014 Henrik Baerbak Christensen, Aarhus University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+package cs.rsa.ts14.Golf.statemachine;
+
+import cs.rsa.ts14.framework.*;
+
+/** Define a ConcreteState for the LineSequenceState
+ * 
+ * @author Henrik Baerbak Christensen, Aarhus University
+ *
+ */
+
+public class EmptyState implements LineSequenceState {
+
+  private int count;
+
+  public EmptyState(int count) {
+    this.count = count;
+  }
+
+  @Override
+  public String lastError() {
+    return "OK";
+  }
+
+  @Override
+  public LineSequenceState transitionOn(LineType lineType) {
+    LineSequenceState state = null;
+    switch( lineType ) {
+    case ASSIGNMENT_LINE:
+    	state = new AssignmentState();
+      break;
+    case COMMENT_LINE:
+    	state = new CommentState();
+      break;
+    case EMPTY_LINE:
+      if ( count == 2 ) {
+        state = new IllegalState("Illegal to have three consecutive empty lines.");
+      } else { 
+        count++;
+        state = this;
+      }
+      break;
+    case WEEKDAY_SPECIFICATION:
+    	state = new WeekDayState();
+      break;
+    case WEEK_SPECIFICATION:
+    	state = new WeekState();
+      break;
+    case WORK_SPECIFICATION:
+    	state = new WorkState();
+      break;
+    default:
+      break;
+    }
+    return state;
+  }
+
+}
---
+++ ts14/src/cs/rsa/ts14/Golf/statemachine/WorkState.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2014 Henrik Baerbak Christensen, Aarhus University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+package cs.rsa.ts14.Golf.statemachine;
+
+import cs.rsa.ts14.framework.*;
+
+/** Define a ConcreteState for the LineSequenceState
+ * 
+ * @author Henrik Baerbak Christensen, Aarhus University
+ *
+ */
+public class WorkState implements LineSequenceState {
+
+  @Override
+  public String lastError() {
+    return "OK";
+  }
+
+  @Override
+  public LineSequenceState transitionOn(LineType lineType) {
+    LineSequenceState state = null;
+    switch( lineType ) {
+    case ASSIGNMENT_LINE:
+      state = new IllegalState("Illegal to have assignment after work line.");
+      break;
+    case COMMENT_LINE:
+    	state = new CommentState();
+      break;
+    case EMPTY_LINE:
+    	state = new EmptyState(1);
+      break;
+    case WEEKDAY_SPECIFICATION:
+    	state = new WeekDayState();
+      break;
+    case WEEK_SPECIFICATION:
+    	state = new WeekState();
+      break;
+    case WORK_SPECIFICATION:
+    	state = new WorkState();
+      break;
+    default:
+      break;
+    }
+    return state;
+  }
+
+}
